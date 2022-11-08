d40770e78d849ededc7830f898d377ed23aa7b82
dededec <daviderenacasado@gmail.com>
2022-11-08 11:31:42 +0100

    answer tracking

--- questionnaire-assessment-frontend/src/components/Question.js
+++ questionnaire-assessment-frontend/src/components/Question.js
@@ -2,7 +2,7 @@ import '../styles/Question.css'
 
 function Question(props) {
 
-
+    var select = document.getElementById('answerSelect');
 
     return (
         <div className="margins">
@@ -10,15 +10,15 @@ function Question(props) {
                 <div class="card-body">
                     <h5 class="card-title">Question no.1/20</h5>
                     <p class="card-text">{props.questionText && props.questionText}</p>
-                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                        <option selected>Pick an answer</option>
+                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" id="answerSelect">
+                        <option value= "" disabled selected>Pick an answer</option>
                         {props.answers && props.answers.map(answer => <option value={answer.answerText}>{answer.answerText}</option>)}
                     </select>
                 </div>
             </div>
 
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button class="btn btn-primary me-md-2" type="button" onClick={props.onSubmit}>Submit</button>
+                <button class="btn btn-primary me-md-2" type="button" onClick={() => props.onSubmit(select.options[select.selectedIndex].value)}>Submit</button>
             </div>
         </div>
     );
