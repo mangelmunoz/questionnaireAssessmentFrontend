88b65209ae3c70d87dc6662e8856c1541b825dca
dededec <daviderenacasado@gmail.com>
2022-11-08 15:07:03 +0100

    quick question fix

--- questionnaire-assessment-frontend/src/pages/questionPage.js
+++ questionnaire-assessment-frontend/src/pages/questionPage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import Question from "../components/Question";
+import Question from "../components/question";
 
 function QuestionPage(props) {
 
